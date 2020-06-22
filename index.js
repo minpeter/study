9f7b711a10e9ebbebed4249f0e0c0685e372f12d
minpeter <kali2005611@gmail.com>
2020-06-22 18:31:23 +0900

    I can drink?_ver 2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const age = prompt("how old are you");
 
-if(age > 18){
-    console.log('you can drink');
+if(age >= 18 && age <= 21){
+    console.log("you can drink but should not");
+}else if(age > 21){
+    console.log("go ahed");
 }else{
-    console.log("you cna't");
+    console.log("you can't");
 }
