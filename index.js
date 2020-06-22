c50a359cc71ba2dac5f2d10a42a302175e5f474f
minpeter <kali2005611@gmail.com>
2020-06-22 15:26:25 +0900

    objInObj

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-//obejct
+//object in object
 const something = {
     name:"minpeter",
-    age:16
+    age:16,
+    minpeterLike:{a:"computer",b:"JS"}
 }
 
 console.log(something.name);
 console.log(something.age);
+console.log(something.minpeterLike.a);
+console.log(something.minpeterLike.b);
