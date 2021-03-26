b8b6bfe32e0a690419e95ce5fb62dcc7d3d7fcdf
minpeter <minpeter>
2021-03-26 18:42:30 +0900

    day4 함수

--- day3/3-4.c
+++ day3/3-4.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-int main(){
-
-    int a[10] = {11,22,33,44,55,66,77,88,99,110};
-    int i;
-
-    for (i=0; i<sizeof(a)/sizeof(int); i++)
-    {
-        a[i] *= 2;
-    }
-
-    for (i=0; i<sizeof(a)/sizeof(int); i++)
-    {
-        printf("%d\n", a[i]);
-    }
-
-    return 0;
-}
+//1이랑 햇갈리지만 0부터시작하는게 맞으니 2번
