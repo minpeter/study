bec62ea9adaf9f4fd9ac7e1645afc6ca83d3d44f
minpeter <minpeter>
2021-03-31 12:15:55 +0900

    day6 F

--- day6-homework/6h-4.c
+++ day6-homework/6h-4.c
@@ -1,8 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
+int ascend(int* Pointer, int count);
+
 int main(){
-    int *Pointer;
     int Arr[] = {4, 2, 6, 1, 8, 10, 7, 9, 5, 3};
-    Pointer = &Arr;
-    for(int i=0; i<)
+    int count = sizeof(Arr)/sizeof(int);
+
+    ascend(Arr, count);
     return 0;
+}
+
+
+int ascend(int* Pointer, int count){
+    int i,j;
+    int tmp;
+    
+    for(i=0; i<count; i++){         
+        for(j=i+1; j<count; j++){  
+            if(*(Pointer+i)>*(Pointer+j)){     
+                tmp = *(Pointer+i);    
+                *(Pointer+i) = *(Pointer+j);
+                *(Pointer+j) = tmp;
+            }
+        }
+    }
+    int num;
+    
+    for(num=0; num<count; num++)      
+        printf("array[%d]: %d \n", num, *(Pointer+num));   
+    return 0;    
 }
