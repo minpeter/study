f5caffa612178f9967f5276be2740c2894a56fbe
minpeter <kali2005611@gmail.com>
2021-05-16 23:17:35 +0900

    char.rs

--- RustWorkspace/dataformat/bool.rs
+++ RustWorkspace/dataformat/bool.rs
@@ -3,5 +3,4 @@ fn main() {
     println!("{}", is_bigger); // false
     is_bigger = 1 < 4;
     println!("{}", is_bigger); //true
-//git commit msg
 }
---
+++ RustWorkspace/dataformat/char.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+    println!("{} and ❤ or 🌹",heart_eyed_cat);
+}
