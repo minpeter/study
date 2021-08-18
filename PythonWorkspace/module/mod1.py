365e258e51186433a80c9ee57d910a377b604005
minpeter <kali2005611@gmail.com>
2021-08-18 15:59:55 +0900

    python module

---
+++ PythonWorkspace/module/mod1.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+#확장자가 *.py인 파일은 전부 모듈이다.
+#import (파일이름) 명령어를 통해 불러올 수 있다.
+
+PI = 3.141592 #mod1.PI 를 사용하여 값을 다른 파일에서 불러올 수 있다.
