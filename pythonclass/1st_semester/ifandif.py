3426bd6e8c80fae30571f83927740d6a4575cb24
minpeter <kali2005611@gmail.com>
2021-08-24 09:43:54 +0900

    pythonclass 파일 학기별로 분리

---
+++ pythonclass/1st_semester/ifandif.py
@@ -0,0 +1,15 @@
+x = int(input("월을 입력하세요: "))
+
+if x >= 3 and x <= 5:
+    print("봄")
+elif x >= 6 and x <= 8:
+        print("여름")
+elif x >= 9 and x <= 11:
+        print("가을")
+elif x == 12 or x == 2 or x == 1:
+        print("겨울")
+else:
+    print("1-12사이에서 입력하세요.")
+
+
+    
--- pythonclass/for.py
+++
@@ -1,4 +0,0 @@
-temp = 0
-for i in range(11):
-    temp = temp + i
-print(temp)
