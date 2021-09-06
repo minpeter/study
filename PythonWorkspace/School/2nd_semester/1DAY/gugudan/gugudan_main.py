e39bf02420b83e37de97ba90362c585801e021d5
minpeter <kali2005611@gmail.com>
2021-09-06 22:42:21 +0900

    pythonworkspace/school/2s/1d 설명추가

--- PythonWorkspace/School/2nd_semester/1DAY/gugudan/gugudan_main.py
+++ PythonWorkspace/School/2nd_semester/1DAY/gugudan/gugudan_main.py
@@ -1,5 +1,9 @@
 import gugudan
 
-for i in range(2,20): # 2부터 19까지 반복
-    print("=" * 20)
-    gugudan.gugudan(i)
+def main():
+    for i in range(9,10):
+        print("=" * 20)
+        gugudan.gugudan(i)
+
+if __name__ == "__main__":
+    main()
